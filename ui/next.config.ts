a58c51fb7a69edacfc5129f3586d9ab4f1985694
Viktor Login <batazor111@gmail.com>
2025-08-13 00:57:08 +0500

    feat: enhance Next.js configuration with new settings and optimizations

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -5,10 +5,33 @@ const isProd = process.env.NODE_ENV === "production"
 const AUTH_URI = process.env.AUTH_URI || "http://127.0.0.1:4433"
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  generateEtags: false,
+  output: 'export',
+  compress: isProd,
+  productionBrowserSourceMaps: isProd,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  transpilePackages: ['@shortlink-org/ui-kit'],
+  compiler: {
+    // ssr and displayName are configured by default
+    emotion: true,
+  },
+  trailingSlash: false,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  bundlePagesRouterDependencies: true,
+  experimental: {
+    webVitalsAttribution: ['CLS', 'FCP', 'FID', 'INP', 'LCP', 'TTFB'],
+    clientTraceMetadata: ['sentry-trace', 'baggage'],
+    // reactCompiler: true,
+    // typedRoutes: true,
   },
   // If you truly need a CDN path for static assets only, set assetPrefix as well,
   // but usually basePath is sufficient for an app mounted at /auth.
